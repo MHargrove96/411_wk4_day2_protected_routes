8e988b7252b32303f93b56894c3df6e3695af95a
MHargrove96 <MichaelH.96@yahoo.com>
2022-05-09 18:47:06 -0500

    useHistory will not import from react-router-dom

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,60 +1,77 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router'
-import {
-  TextField,
-  Button,
-  Container
-} from '@material-ui/core'
+import React, { useState, useEffect, Component } from "react";
+import { Redirect } from "react-router";
+import { useHistory } from "react-router-dom";
+import cookie from "cookie";
+import { TextField, Button, Container } from "@material-ui/core";
 
-class App extends Component {
-  state = {
-    username: '',
-    password: ''
-  }
+function App() {
+  // const [state, setState] = useState({
+  //   username: "",
+  //   password: "",
+  // });
 
-  handleTextChange = (e) => {
-    const state = { ...this.state }
-    state[e.target.name] = e.target.value
-    this.setState(state)
-  }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
 
-  login = (e) => {
-    e.preventDefault()
+  useEffect(() => {
+    console.log(username);
+    console.log(password);
+  }, [username, password]);
+
+  const handleUsername = (e) => {
+    // const state = { ...state };
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    // const state = { ...state };
+    setPassword(e.target.value);
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+
     // set cookie here
+    // cookie.serialize("loggedIn", true);
+    // const cookies = cookie.parse(document.cookie);
+    // console.log(cookie);
+    // history.push("/");
     // set loggedIn = true and max-age = 60*1000 (one minute)
 
-    window.location.replace("/")
-  }
+    // window.location.replace("/");
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <Container maxWidth="sm">
-          <form className="login-form" onSubmit={this.login}>
-            <TextField
-              required
-              onChange={this.handleTextChange}
-              value={this.state.username}
-              name="username"
-              label="Username"
-              type="text" />
-            <TextField
-              required
-              onChange={this.handleTextChange}
-              value={this.state.password}
-              name="password"
-              label="Password"
-              type="password" />
-            <Button
-              type="submit"
-              className="login-button"
-              variant="contained"
-              color="primary">Login</Button>
-          </form>
-        </Container>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Container maxWidth="sm">
+        <form className="login-form" onSubmit={login}>
+          <TextField
+            onChange={handleUsername}
+            value={username}
+            name="username"
+            label="Username"
+            type="text"
+          />
+          <TextField
+            onChange={handlePassword}
+            value={password}
+            name="password"
+            label="Password"
+            type="password"
+          />
+          <Button
+            type="submit"
+            className="login-button"
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </form>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
